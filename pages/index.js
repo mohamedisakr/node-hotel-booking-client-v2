5624b7f2ed16c8c62c8c42b03e1dfb8f10f8e59b
mohamedisakr <mohamedisakr@gmail.com>
2022-01-23 10:33:22 +0200

    building the header component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* header */}
+      <Header />
       {/* banner (hero) */}
     </div>
   )
